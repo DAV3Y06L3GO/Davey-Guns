a15f1b658ef9955622b40e2e02b784fc6c0b3969
David Lofton <davey06lego@gmail.com>
2024-01-29 17:53:09 -0600

    added new creative tab

--- src/main/java/com/davey/daveyguns/DaveyGuns.java
+++ src/main/java/com/davey/daveyguns/DaveyGuns.java
@@ -1,8 +1,13 @@
 package com.davey.daveyguns;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -20,16 +25,33 @@ public class DaveyGuns
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
+    // Creative tab
+public static final CreativeModeTab GROUP = new CreativeModeTab.Builder(CreativeModeTab.Row.TOP, 1)
+            .title(Component.translatable("itemGroup.davey_guns.creativeTab"))
+            .icon(() -> {  return new ItemStack(Items.POTATO);  })
+            .displayItems((smth, tab) -> {
+                tab.accept(Items.ACACIA_LOG);
+            })
+            .build();
+
+
     public DaveyGuns()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.register(this);
+
+
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-
+        modEventBus.addListener(this::ClientSetup);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
+    }
+
+    private void ClientSetup(final FMLClientSetupEvent event) {
+
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
@@ -42,17 +64,5 @@ public class DaveyGuns
 
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
 
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-
-        }
-    }
 }
---
+++ src/main/java/com/davey/daveyguns/registry/ModItems.java
@@ -0,0 +1,15 @@
+package com.davey.daveyguns.registry;
+
+import com.davey.daveyguns.DaveyGuns;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public final class ModItems {
+
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, DaveyGuns.MOD_ID);
+
+
+
+}
